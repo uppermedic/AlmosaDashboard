256403feadd6731a78ebd21048cf56759188f819
Tarek <tarek@uppermedic.com>
2021-07-11 23:32:27 +0700

    hot fix

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -52,7 +52,7 @@ class DoctorController extends Controller
 
     public function filter(Request $request)
     {
- $section_id = (int)$request->section_id;
+        $section_id = (int)$request->section_id;
 
         $heads = Section::find($section_id)->doctors;
         $heads = $heads->load('translations');
@@ -69,8 +69,8 @@ class DoctorController extends Controller
             'en'=>Helper::toTranslation($heads->translations),
 
         ];
-
-        $doctors = Doctor::whereSection_id($section_id)->with('section','translations')->paginate(12);
+//        $doctors = Doctor::whereSection_id($section_id)->with('section','translations')->paginate(12);
+        $doctors = Doctor::whereSection_id($section_id)->with('translations')->paginate(12);
 
         $doctors_data['current_page'] = $doctors->currentPage();
         $doctors_data['next_page_url'] = $doctors->nextPageUrl();
@@ -79,19 +79,19 @@ class DoctorController extends Controller
         $doctors_data['last_page'] = $doctors->lastPage();
         $doctors_data['data'] = [];
         foreach ($doctors->items() as $doctor) {
-            array_push($doctors_data['data'],[
+            $doctors_data['data'][] = [
                 'image' => Voyager::image($doctor->image),
-                'id'=>$doctor->id,
+                'id' => $doctor->id,
                 'ar' => [
                     'name' => $doctor->name,
-                    'slug'=>$doctor->slug,
+                    'slug' => $doctor->slug,
                     'title' => $doctor->title,
                     'qualifications' => $doctor->qualifications,
-                    'current_positions'=>$doctor->current_positions,
+                    'current_positions' => $doctor->current_positions,
                 ],
-                'en'=>Helper::toTranslation($doctor->translations),
+                'en' => Helper::toTranslation($doctor->translations),
 
-            ]);
+            ];
         }
 	$page = Page::where('id','=',9)->firstOrFail();
 
